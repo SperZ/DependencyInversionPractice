d012b1459cabfed5af2a0f69cb7a7586503e31a8
SperZ <64809040+SperZ@users.noreply.github.com>
2021-03-30 00:06:26 -0400

    Add files via upload

---
+++ CarSales.UI/Factory.cs
@@ -0,0 +1,47 @@
+﻿using CarSales.ConsoleApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSales.UI
+{
+    public static class Factory
+    {
+        public static IVehicle CreateVehicle()
+        {
+            return new Car();
+        }
+
+        public static IPerson CreateSalesman()
+        {
+            return new SalesMan();
+        }
+
+        public static IPerson CreateCustomer()
+        {
+            return new Customer();
+        }
+
+        public static IVehicleInventory CreateVehicleInventory()
+        {
+            return new VehicleInventory();
+        }
+
+        public static ISalesReciept CreateSalesReciept()
+        {
+            return new SalesReciept();
+        }
+
+        public static IPersonInventory CreateCustomerInventory()
+        {
+            return new CustomerInventory();
+        }
+
+        public static IPersonInventory CreateEmployeeInventory()
+        {
+            return new SaleEmployeeInventory();
+        }
+    }
+}
